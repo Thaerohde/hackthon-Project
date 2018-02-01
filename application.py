c008c93ecac21d2f77eab1cd4d46282f4cf55353
mohammad sawas <mohammad.sawas2016@gmail.com>
2018-02-01 17:01:45 +0000

    last version

---
+++ application.py
@@ -0,0 +1,179 @@
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions
+from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import apology, login_required
+from data_base import User_Data
+
+# Configure application
+app = Flask(__name__)
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+sql_man = User_Data()
+
+@app.route("/",methods=["GET", "POST"])
+def index():
+    events = sql_man.get_events()
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+    # make this clean
+    # Forget any user_id
+
+    session.clear()
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        # Ensure username was submitted
+        if not username:
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        if not password:
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = sql_man.get_user_info(username)
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        flash("welcome " + username)
+        return redirect("/start")
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+        email = request.form.get("email")
+
+        # Ensure username was submitted
+        if not request.form.get("email"):
+            return apology("Missing the E-mail")
+        if not request.form.get("username"):
+            return apology("Missing the name")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password")
+        elif not request.form.get("confirmation"):
+            return apology("must provide password")
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("not match")
+
+        # Insert the data of the seller
+        hash = generate_password_hash(password)
+
+        # Insert the data of the new user
+        newUser = sql_man.create_user(username, hash, email)
+        if not newUser:
+            return apology("You are Already registered", 400)
+
+        # Remember which user has logged in
+        session["id"] = newUser
+
+        # Redirect user to register page
+        flash("Welcome " + username)
+        return redirect("/")
+    else:
+        return render_template("register.html")
+
+
+@app.route("/start", methods=["GET", "POST"])
+def start():
+    if request.method == "GET":
+        return render_template("start.html")
+    return render_template("index.html")
+
+@app.route("/create", methods=["GET", "POST"])
+@login_required
+def createvent():
+    """Allow the user to create events from a list"""
+    if request.method == "POST":
+        eventName = request.form.get("eventName")
+        eventDate = request.form.get("eventDate")
+        eventPlace = request.form.get("eventPlace")
+        eventType = request.form.get("eventType")
+
+        if not eventName:
+            return apology("please enter the event name")
+
+        if not eventDate:
+            return apology("please enter the event date")
+
+        if not eventPlace:
+            return apology("please enter the event place")
+
+        if not eventPlace:
+            return apology("please enter the event type")
+        # session["id"] = id
+        new_event = sql_man.create_new_event(session["id"], eventDate, eventPlace, eventType, eventName)
+        events = sql_man.get_events()
+        return render_template("start.html")
+    else:
+        return render_template("create.html")
+
+@app.route("/joinevent", methods=["GET", "POST"])
+@login_required
+def joinevent():
+    if request.method == "POST":
+        events = sql_man.get_available_events()
+        for event in events:
+            print(event)
+        return render_template("index.html", events = events)
+    return render_template("index.html")
+
+@app.route("/eventspage", methods=["GET", "POST"])
+@login_required
+def eventspage():
+    events = sql_man.get_available_events()
+    if request.method == "GET":
+        return render_template("eventspage.html", events = events)
+    return render_template("start.html")
+
---
+++ data_base.py
@@ -0,0 +1,33 @@
+from cs50 import SQL
+from flask import  request
+from flask_session import Session
+from flask import session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+class User_Data:
+    def __init__ (self):
+        # Configure CS50 Library to use SQLite database
+        self.db = SQL("sqlite:///sport.db")
+
+    def create_user(self, username, hash, email):
+        return self.db.execute("INSERT INTO users (username, hash, email) VALUES(:username,:hash, :email)",
+                                    username=username,hash= hash, email=email)
+
+    def get_user_info(self, username):
+        return self.db.execute("SELECT * FROM users WHERE username = :username", username=username)
+
+    def check_user(self, email):
+        return self.db.execute("SELECT * FROM users WHERE email = :email", email = email)
+
+    def create_new_event(self, id, eventDate, eventPlace, eventType, eventName):
+        return self.db.execute("INSERT INTO events (id ,date, place, type, eventname, created, joined) VALUES (:id, :date, :place, :type, :eventname, :true, :false)",
+                                        id=id, date = eventDate, place = eventPlace ,type = eventType, eventname = eventName, true=1, false=0)
+
+    def get_events(self):
+        return self.db.execute("SELECT * FROM events WHERE created")
+
+    def get_available_events(self):
+        return self.db.execute("SELECT *FROM events  WHERE participant = 1 group by eventname")
+
+"""symbol = db.execute("SELECT sum (shares) as shares, symbol, name , price, sum(total) as total FROM history WHERE id =:id group by symbol,name",
+    id = session["user_id"])"""
